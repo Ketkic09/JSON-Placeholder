2f474bee0e6b815037886e3cee9347ee332fa02f
Ketkic09 <ketki.chavan22@gmail.com>
2025-01-30 15:41:00 +0530

    Interceptor implementation done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MAX_RETRIES = 2
 
 app.use(express.json());
 
@@ -20,55 +21,84 @@ axiosInstance.interceptors.response.use(null,async(error)=>{
     
     if(retries>=MAX_RETRIES) return Promise.reject(error);
 
-    retries+=1
-    error.config.__retryCount = retries
+    retries+=1  
+    error.config.__retryCount = retries //saving the retry number
 
-    const delay = Math.pow(2,retries-1)*1000
+    const delay = Math.pow(2,retries-1)*1000  //exponential backoffs
+
+    console.log(`Retry attempt in ${retries} in ${delay/1000}`)
+
+    await new Promise(resolve=>setTimeout(resolve,delay)) //delay before new try
+
+    return axiosInstance(error.config) //retrying the API call
 
 })
 
 app.get('/fetch-users', async (req, res) => {
     try {
-        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
-        console.log(data);
+        const { data } = await axiosInstance.get(process.env.BASE_URL);
         return res.status(200).json(data);
     } catch (error) {
         console.error(error);
-        return res.status(error.status).json({ error: 'Internal Server Error' });
+        if(error.response){
+            return res.status(error.response.status).json({status:error.response.status,error:error.response.statusText})
+        }
+        return res.status(error.status||500).json({ error: error.statusText  });
     }
 });
 
 app.post('/post-user',async(req,res)=>{
     try{
-    
-       const response =  await axios.post("https://jsonplaceholder.typicode.com/users",req.body)
+       const response =  await axiosInstance.post(process.env.BASE_URL,req.body)
         return res.status(response.status).json(response.data)
     }catch(error){
         console.error(error.message);
-        return res.status(error.status).json({ error: 'Internal Server Error' });
+        if (error.response) {
+            return res.status(error.response.status).json({
+                status:error.response.status || 500, 
+                error: error.response.statusText || "Request Failed"
+                
+            });
+        }
+        return res.status(error.status||500).json({ error: error.statusText || "Internal Sever Error"});
     }
 })
 
 app.put('/update-user/:id',async(req,res)=>{
     const {id} = req.params;
-    console.log('url: ', process.env.BASE_URL+`/${id}`)
     try{
-        const response = await axios.put(process.env.BASE_URL+`/${id}`,req.body)
+        const userPresent = await axiosInstance.get(process.env.BASE_URL+`/${id}`) //to check if the user to be deleted is present
+        const response = await axiosInstance.put(process.env.BASE_URL+`/${id}`,req.body)
         return res.status(response.status).json(response.data)
     }catch(error){
-        console.log(error)
-        return res.status(error.status).json({error:error.statusText})
+        console.log(error.response)
+        if (error.response) {
+            return res.status(error.response.status).json({
+                status:error.response.status || 500, 
+                error: error.response.statusText || "Request Failed"
+                
+            });
+        }
+        return res.status(error.status||500).json({error:error.statusText || "Internal Sever Error"})
     }
 })
 
 app.delete('/delete-user/:id',async(req,res)=>{
     const {id} = req.params;
+    console.log("URL: ",process.env.BASE_URL+`/${id}`)
     try{
-        const response = await axios.delete(process.env.BASE_URL+`/${id}`)
-        return res.status(response.status).json(response.data)
+
+        const userPresent = await axiosInstance.get(process.env.BASE_URL+`/${id}`) //to check if the user to be deleted is present
+        const response = await axiosInstance.delete(process.env.BASE_URL+`/${id}`)
+        return res.status(response.status).json(`Deleted record of ID ${id}`)
     }catch(error){
-        console.log(error)
-        return res.status(error.status).json({error:error.statusText})
+        if (error.response) {
+            return res.status(error.response.status).json({
+                status:error.response.status || 500, 
+                error: error.response.statusText || "Request Failed"
+                
+            });
+        }
+        return res.status(error.status||500).json({error:error.statusText || "Internal Sever Error"})
     }
 })
-
